4fcee024f3abd38585531bf52fd73602de3fb5fa
myronkoch-dev <myronkoch.dev@gmail.com>
2023-07-04 00:24:57 -0400

    added buttons for GPT3.5 and GPT4-default

--- popup.js
+++ popup.js
@@ -1,3 +1,23 @@
-document.getElementById('openButton').addEventListener('click', function() {
+function addEventListenerOnce(elementId, event, func) {
+  var element = document.getElementById(elementId);
+  if (element && !element[event + 'ListenerAdded']) {
+    element.addEventListener(event, func);
+    element[event + 'ListenerAdded'] = true;
+  }
+}
+
+addEventListenerOnce('openButton', 'click', function() {
   chrome.tabs.create({ url: 'https://chat.openai.com/?model=gpt-4-plugins' });
+});
+
+addEventListenerOnce('openButton4Browsing', 'click', function() {
+  chrome.tabs.create({ url: 'https://chat.openai.com/?model=gpt-4-browsing' });
+});
+
+addEventListenerOnce('openButton4Default', 'click', function() {
+  chrome.tabs.create({ url: 'https://chat.openai.com/?model=gpt-4' });
+});
+
+addEventListenerOnce('openButton35Turbo', 'click', function() {
+  chrome.tabs.create({ url: 'https://chat.openai.com/?model=text-davinci-002-render-sha' });
 });
